8629f8b8490d3d224c3cb089d63ca6dab7f0b96d
yunweneric <yunweneric@gmail.com>
2021-01-14 16:28:22 +0100

    added nav component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Nav from "./nav";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+      <Nav/>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Qui
+          veritatis sed provident repudiandae, earum officiis voluptatem,
+          aspernatur pariatur tempore ducimus error alias beatae itaque rem sit
+          nisi magnam deserunt culpa.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
---
+++ src/nav.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function Nav() {
+  return (
+    <div>
+      <div>
+        <h2> Nav bar</h2>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
